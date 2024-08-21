80abdc14efabd5dd808c2e5ca6b90c8025720759
taariqelliott <codebytaariq@gmail.com>
2024-08-21 03:11:56 -0400

    added mantine ui for styling and easy components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        "realBlue": "#0000fe",
+        "realBlue": "#0000ff",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
