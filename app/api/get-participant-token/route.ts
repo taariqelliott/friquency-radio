bedb18b35dd3988af98446953d3adc4165f7263c
Taariq Elliott <codebytaariq@gmail.com>
2024-09-24 02:58:23 -0400

    test 4

--- app/api/get-participant-token/route.ts
+++ app/api/get-participant-token/route.ts
@@ -4,10 +4,14 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
   const room = req.nextUrl.searchParams.get("room");
   const username = req.nextUrl.searchParams.get("username");
-
-  if (!room || !username) {
+  if (!room) {
     return NextResponse.json(
-      { error: 'Missing "room" or "username" query parameter' },
+      { error: 'Missing "room" query parameter' },
+      { status: 400 }
+    );
+  } else if (!username) {
+    return NextResponse.json(
+      { error: 'Missing "username" query parameter' },
       { status: 400 }
     );
   }
@@ -23,14 +27,9 @@ export async function GET(req: NextRequest) {
     );
   }
 
-  // Generate an access token for the user
   const at = new AccessToken(apiKey, apiSecret, { identity: username });
-  at.addGrant({
-    room,
-    roomJoin: true,
-    canPublish: true,
-    canSubscribe: true,
-  });
+
+  at.addGrant({ room, roomJoin: true, canPublish: true, canSubscribe: true });
 
   return NextResponse.json({ token: await at.toJwt() });
 }
