e7b0070f4d6c0e1f653a048f2056cfbf35a00f92
Taariq Elliott <codebytaariq@gmail.com>
2024-10-04 10:44:28 -0400

    Refactor Home component to update page title

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default async function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-dvh p-24">
       <h1 className="text-4xl font-bold mb-8 text-center text-pink-500">
-        Friquency Radio
+        Friquency Radio (Demo Test)
       </h1>
       <div className="flex">
         {" "}
